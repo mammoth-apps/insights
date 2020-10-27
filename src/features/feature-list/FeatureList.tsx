1f650e3f1e2759ec3050255519207684130683b0
Ryan Bruns <ryan.bruns7@gmail.com>
2020-10-27 07:51:06 -0500

    going to look into code splitting with the router later

---
+++ src/features/feature-list/FeatureList.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const FeatureList = () => {
+  return <div>Feature List</div>
+}
--- src/router/app.routes.tsx
+++ src/router/app.routes.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { BasePage } from '../features/base/BasePage'
-import { AuthenticatedRoute } from '../features/auth/AuthenticatedRoute'
 import { InsightRoute } from './routes'
 import { Login } from '../features/login/Login'
+import { CompanyDetails, FeatureList } from '../features'
+
+// * TODO: Look into code splitting. Since the app doesn't need to load on mount it can wait until it's needed might be fun to try.
 
 export const AppRoutes: React.FC<{}> = (): JSX.Element => {
   return (
@@ -11,6 +13,8 @@ export const AppRoutes: React.FC<{}> = (): JSX.Element => {
       <Switch>
         <Route exact path={InsightRoute.Default} component={BasePage} />
         <Route exact path={InsightRoute.Login} component={Login} />
+        <Route exact path={InsightRoute.Features} component={FeatureList} />
+        <Route exact path={InsightRoute.Company} component={CompanyDetails} />
         {/* <AuthenticatedRoute path={InsightRoute.App} component={ProtectedRoutes} /> */}
       </Switch>
     </BrowserRouter>
