c9aea179b972fe3c26a6edc720525035c12efdd8
Ryan Bruns <ryan.bruns7@gmail.com>
2020-11-14 08:11:04 -0600

    need to break this down still but it's better than anything I can make

--- src/router/app-protected.routes.tsx
+++ src/router/app-protected.routes.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import { InsightApp } from '../features/app/InsightApp'
 import { BudgetSelectionPage } from '../features/budget/BudgetSelectionPage'
 import { InsightRoute } from './routes'
 
@@ -7,7 +8,7 @@ export const ProtectedAppRoutes: React.FC = (): JSX.Element => {
   return (
     <Switch>
       <Route exact path={InsightRoute.App} component={BudgetSelectionPage} />
-      {/* <Route path={InsightRoute.BudgetHub} component={BudgetRouter} /> */}
+      <Route path={InsightRoute.BudgetHub} component={InsightApp} />
     </Switch>
   )
 }
